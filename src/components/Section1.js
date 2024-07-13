f5fa226274b4a36268b260124903f2bdf56aa8d0
PRASANTH2862001 <prashanth2862001@gmail.com>
2024-07-13 14:25:02 +0530

    project uploaded

---
+++ src/components/Section1.js
@@ -0,0 +1,16 @@
+function Section1() {
+    return (
+      <nav>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>
+            Home | </li>
+          <li>Products | </li>
+          <li><a>About |</a> </li>
+          <li><a>Contact |</a> </li>
+        </ul>
+      </nav>
+    )
+  }
+
+  export default Section1
---
+++ src/components/Section2.js
@@ -0,0 +1,11 @@
+function Section2() {
+    return (
+      <div class="searchbar">
+  
+        <input type="text" placeholder="Search" />
+  
+      </div>
+    )
+  
+  }
+  export default Section2
